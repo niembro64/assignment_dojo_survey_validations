f033c3298ef587ff3944e8b4eda99ae2e49a1803
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-12 15:13:58 -0400

    updating

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace assignment_dojo_survey_validations.Models
+{
+
+  public class User
+  {
+
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int Number { get; set; }
+
+
+  }
+
+}
